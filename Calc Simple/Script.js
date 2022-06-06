d0f90b89a8850be3331b2480f6bf2a9d4cd7526d
hero <58014126+New-Era-hero@users.noreply.github.com>
2022-06-06 16:32:01 -0400

    Add files via upload

--- Calc Simple/Script.js
+++ Calc Simple/Script.js
@@ -5,21 +5,36 @@ let n1 = 0;
 let n2 = 0;
 let op = 0;
 
-const sumar = (n1, n2) => {
-    return parseInt(n1 + n2);
-};
+class Calculadora {
+    sumar(n1, n2) {
+        return parseInt(n1 + n2);
+    };
 
-const restar = (n1, n2) => {
-    return parseInt(n1 - n2);
-};
+    restar(n1, n2) {
+        return parseInt(n1 - n2);
+    };
 
-const multiplicar = (n1, n2) => {
-    return parseInt(n1 * n2);
-};
+    multiplicar(n1, n2) {
+        return parseInt(n1 * n2);
+    };
 
-const dividir = (n1, n2) => {
-    return parseInt(n1 / n2);
-};
+    dividir(n1, n2) {
+        return parseInt(n1 / n2);
+    };
+
+    potenciar(n1, n2) {
+        return parseInt(n1 ** n2);
+    }
+    raiz2(n1) {
+        n1 = Math.sqrt(n1);
+        return n1;
+    }
+    raiz3(n1) {
+        n1 = Math.cbrt(n1);
+        return n1;
+    }
+}
+const calculadora = new Calculadora();
 
 const start = (f) => {
     if (f == 1) {
@@ -33,7 +48,10 @@ const start = (f) => {
 1: sumar, 
 2: restar, 
 3: multiplicar, 
-4: dividir`);
+4: dividir
+5: potencia
+6: raís cuadrada
+7: raís cubica`);
     ope();
 }
 
@@ -42,19 +60,19 @@ ope = () => {
         if (op == 1) {
             n1 = parseInt(prompt("Ingresa el primer numero"));
             n2 = parseInt(prompt("Ingresa el segundo numero"));
-            alert(`${n1} + ${n2} = ${sumar(n1, n2)}`);
+            alert(`${n1} + ${n2} = ${calculadora.sumar(n1, n2)}`);
             start();
 
         } else if (op == 2) {
             n1 = parseInt(prompt("Ingresa el primer numero"));
             n2 = parseInt(prompt("Ingresa el segundo numero"));
-            alert(`${n1} - ${n2} = ${restar(n1, n2)}`);
+            alert(`${n1} - ${n2} = ${calculadora.restar(n1, n2)}`);
             start(0);
 
         } else if (op == 3) {
             n1 = parseInt(prompt("Ingresa el primer numero"));
             n2 = parseInt(prompt("Ingresa el segundo numero"));
-            alert(`${n1} * ${n2} = ${multiplicar(n1, n2)}`);
+            alert(`${n1} * ${n2} = ${calculadora.multiplicar(n1, n2)}`);
             start(0);
 
         } else if (op == 4) {
@@ -66,15 +84,29 @@ ope = () => {
                 start(0);
 
             } else {
-                alert(`${n1} / ${n2} = ${dividir(n1, n2)}`);
+                alert(`${n1} / ${n2} = ${calculadora.dividir(n1, n2)}`);
                 start(0);
             }
 
+        } else if (op == 5) {
+            n1 = parseInt(prompt("Ingresa el primer numero"));
+            n2 = parseInt(prompt("Ingresa el segundo numero"));
+            alert(`${n1} ^ ${n2} = ${calculadora.potenciar(n1, n2)}`);
+            start(0);
+
+        } else if (op == 6) {
+            n1 = parseInt(prompt("Ingresa un numero"));
+            alert(`raíz cuadrada de ${n1} = ${calculadora.raiz2(n1)}`);
+            start(0);
+        } else if (op == 7) {
+            n1 = parseInt(prompt("Ingresa un numero"));
+            alert(`raíz cubica de ${n1} = ${calculadora.raiz3(n1)}`);
+            start(0);
         } else {
-            alert("Bye");
-            alert("PD: recarga la pagina cuando quieras hacer una operacion");
             break;
         }
     }
+    alert("Bye");
+    alert("PD: recarga la pagina cuando quieras hacer una operacion");
 }
 start(1);
