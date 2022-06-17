bc9a2e2c30def12302e94924822b0433abe6b67a
hero <58014126+New-Era-hero@users.noreply.github.com>
2022-06-17 11:24:27 -0400

    Add files via upload

--- Aprovado-Reprovado/script.js
+++ Aprovado-Reprovado/script.js
@@ -1,52 +1,45 @@
-//Funciones de registro Console
+let materias = {
+    fisica: [90, 6, 4, "fisica"],
+    matematica: [69, 8, 2, "matematica"],
+    logica: [70, 9, 4, "logica"],
+    programacion: [85, 10, 4, "programacion"],
+    quimica: [90, 9, 4, "quimica"],
+    calculo: [94, 7, 3, "calculo"],
+    bbdd: [93, 6, 3, "bbdd"],
+    algebra: [56, 7, 1, "algebra"],
+};
 
-//limpia la consola
-console.clear();
-//basicamente una condicional si es verdadera no sucedera nada pero si es falsa indicara un error
-console.assert(5 > 2);
-//mensaje de error
-console.error("console error");
-//mensaje de informacion
-console.info("mensaje de informacion");
-//mensaje de depuracion
-console.log("mensaje de depuracion");
-//mensaje de advertencia
-console.warn("cuidado bru");
-//pequeña tabla en la consola
-console.table([1, 2, 3, 4, 5, 6, 7, 8, 9]);
-//similar a console.table pero de una manera un tanto diferente
-console.dir([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+const aprove = () => {
+    for (materia in materias) {
+        let asistencias = materias[materia][0];
+        let promedio = materias[materia][1];
+        let practicas = materias[materia][2];
 
-//Funciones de conteo
+        console.log(`${materias[materia][3]}`);
+        // asistencias
+        if (asistencias >= 90)
+            console.log("    %cBuenas asistencias", "color:green");
+        else {
+            console.log("    %Falta de asistencias", "color:red");
+        }
+        //promedio
+        if (promedio >= 7) console.log("    %cPromedio suficiente", "color:green");
+        else {
+            console.log("    %cPromedio inaceptable", "color:red");
+        }
+        //practicas
+        if (practicas >= 3)
+            console.log("    %cPracticas Suficientes", "color:green");
+        else {
+            console.log("    %cPracticas insuficientes", "color: red");
+        }
+        //Aprovado?
+        if (asistencias >= 90 && promedio >= 7 && practicas >= 3)
+            console.log("    %cAprovado Felicidades", "color:green");
+        else {
+            console.log("    %cReprovado lo sentimos", "color:red");
+        }
+    }
+};
 
-//sirve para saber la cantidad de veces que esta misma funcion fue ejecutada
-console.count();
-console.count();
-console.count();
-console.count();
-console.count();
-//puedes resetear el count con esta pequeña funcion
-console.countReset();
-
-//Funciones de agrupacion
-
-//grupo en la misma consola en estado abierto
-console.group("Neshhero");
-//eliminar un grupo
-console.groupEnd("Neshhero");
-//grupo en la misma consola en estado cerrado
-console.groupCollapsed("hero");
-
-//Funciones de temporizacion
-
-//inicia un conteo...hace de contador
-console.time();
-//para ver ese conteo
-console.timeLog();
-//eliminar el contador
-console.timeEnd();
-
-console.clear();
-
-//cambiar color del texto, basicamente darle estilo pero debes ingresar %c antes de escribir el nombre...PD: luego del pading si quieres hacer que tenga un tamaño mas grande pon mas pixeles(px) al lado separado con un espacio para indicar el weight y height
-console.log(`%cNeshhero`, `color:blue;background:white;padding:20px 100px 25px;border: 3px solid blue;`);
+aprove();
